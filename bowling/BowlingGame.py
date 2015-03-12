0041e89ea49f2721dfb9793104f392f785147419
Jesse Adametz <jadametz@findthebest.com>
2015-03-12 12:35:42 -0700

    Bowling: impl - keep track of all rolls to be scored later. Spare test still fails

--- bowling/BowlingGame.py
+++ bowling/BowlingGame.py
@@ -1,9 +1,10 @@
 class BowlingGame(object):
     def __init__(self):
         self.pins_toppled = 0
+        self.rolls = []
 
     def roll(self, pins_toppled):
-        self.pins_toppled += pins_toppled
+        self.rolls.append(pins_toppled)
 
     def score(self):
-        return self.pins_toppled
+        return sum(self.rolls)
