1e8f20b20b23d4add6b9d4d7dc954d65d0fa9567
Jesse Adametz <jadametz@findthebest.com>
2015-03-12 12:26:15 -0700

    Bowling: first test - all gutters return 0

---
+++ bowling/TestBowlingGame.py
@@ -0,0 +1,16 @@
+import unittest
+from BowlingGame import BowlingGame
+
+
+class TestBowlingGame(unittest.TestCase):
+    def setUp(self):
+        self.game = BowlingGame()
+
+    def testAllGuttersReturns0(self):
+        for _ in range(20):
+            self.game.roll(0)
+        self.assertEquals(0, self.game.score())
+
+
+if __name__ == '__main__':
+    unittest.main()
