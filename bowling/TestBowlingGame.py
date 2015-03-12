ba0511a122f88999befa9382dcad837564c8bf07
Jesse Adametz <jadametz@findthebest.com>
2015-03-12 12:43:44 -0700

    Bowling: test - add roll_spare() and roll_times() to remove unnecessary comments

--- bowling/TestBowlingGame.py
+++ bowling/TestBowlingGame.py
@@ -7,23 +7,27 @@ class TestBowlingGame(unittest.TestCase):
         self.game = BowlingGame()
 
     def testAllGuttersReturns0(self):
-        for _ in range(20):
-            self.game.roll(0)
+        self.roll_times(0, 20)
         self.assertEquals(0, self.game.score())
 
     def testAll1sReturns20(self):
-        for _ in range(20):
-            self.game.roll(1)
+        self.roll_times(1, 20)
         self.assertEquals(20, self.game.score())
 
     def testItHandlesSpares(self):
-        self.game.roll(1)
-        self.game.roll(9)
+        self.roll_spare()
         self.game.roll(5)
 
-        for _ in range(17):
-            self.game.roll(0)
+        self.roll_times(0, 17)
         self.assertEquals(20, self.game.score())
+
+    def roll_spare(self):
+        self.game.roll(1)
+        self.game.roll(9)
+
+    def roll_times(self, pins_toppled, times):
+        for _ in range(times):
+            self.game.roll(pins_toppled)
 
 
 if __name__ == '__main__':
