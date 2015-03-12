b2c514a0dc81528b2faa08a11fe25df5972fb900
Jesse Adametz <jadametz@findthebest.com>
2015-03-12 12:47:33 -0700

    Bowling: impl - strike test passing

--- bowling/BowlingGame.py
+++ bowling/BowlingGame.py
@@ -10,12 +10,19 @@ class BowlingGame(object):
         roll = 0
         score = 0
         for frame in range(0, 10):
-            if self.is_spare(roll):
+            if self.is_strike(roll):
+                score += 10 + self.rolls[roll + 1] + self.rolls[roll + 2]
+                roll += 1
+            elif self.is_spare(roll):
                 score += 10 + self.rolls[roll + 2]
+                roll += 2
             else:
                 score += self.rolls[roll] + self.rolls[roll + 1]
-            roll += 2
+                roll += 2
         return score
 
     def is_spare(self, roll):
         return self.rolls[roll] + self.rolls[roll + 1] == 10
+
+    def is_strike(self, roll):
+        return self.rolls[roll] == 10
